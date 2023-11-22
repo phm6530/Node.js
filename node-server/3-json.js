7ca26bbb27e7624e9824908684ff3898b8269421
hm <squirrel309@naver.com>
2023-11-23 01:59:45 +0900

    23/11/23

--- node-server/3-json.js
+++ node-server/3-json.js
@@ -24,14 +24,34 @@ db.connect((err) => {
 });
 
 // GET 요청 처리
-app.get('/cols', (req, res) => {
-  let sql = `SELECT * FROM TEST`;
-  db.query(sql, (err, results, fields) => {
+app.get('/cols', async (req, res) => {
+  let sql = `SELECT count(*) as total FROM TEST`;
+  db.query(sql,  async (err, results, fields) => {
     if (err) {
       console.error(err.message);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-    res.json(results);
+    const page = req.query.page; // 페이지 번호
+    const pageSize = 10; // 한 페이지에 표시될 아이템 수
+
+    const offset = (page - 1) * pageSize;
+
+    const tenPage = `SELECT * FROM TEST LIMIT ${offset}, ${pageSize}`;
+    console.log(tenPage);
+
+    db.query(tenPage , (tenError, Tenresult)=> {
+      if(tenError){
+        console.log(tenError.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      const duobleResult = {
+        total : results[0].total ,
+        data : Tenresult 
+      }
+      res.json(duobleResult);
+    });
+    
   });
 });
 
@@ -48,8 +68,23 @@ app.post('/test', (req, res) => {
     res.json(results);
   });
   // 여기서 데이터를 사용하거나 DB에 저장할 수 있습니다.
-  
 });
+
+// POST 요청 처리
+app.post('/delete', (req, res) => {
+  const data = req.body;
+  console.log('POST : ', data.id);
+  let sql = `delete from test where id = '${JSON.stringify(data.id)}'`;
+    db.query(sql, (err, results, fields) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.json(results);
+  });
+  // 여기서 데이터를 사용하거나 DB에 저장할 수 있습니다.
+});
+
 
 app.listen(port, () => {
   console.log(`서버가 ${port} 포트에서 실행 중입니다.`);
