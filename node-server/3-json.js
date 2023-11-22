7f918902cf1ba113bdba4b7bae571a2ae9a204ac
hm <squirrel309@naver.com>
2023-11-22 18:30:58 +0900

    23/11/22

--- node-server/3-json.js
+++ node-server/3-json.js
@@ -1,22 +1,56 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
-const courses = [
-  {name: 'HTML1'},
-  {name: 'HTML2'},
-  {name: 'HTML3'},
-  {name: 'HTML4'}
-]
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-  const url = req.url; // JSON 받기
-  const method = req.method;
-  if(url === '/cols'){
-    if(method === 'GET'){
-        res.writeHead(200, {'Content-Type' : 'application/json'});
-        res.end(JSON.stringify(courses));
-    }
-  }
+app.use(bodyParser.json());
+app.use(express.static('public')); // 'public' 폴더에 있는 정적 파일을 제공
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '4701',
+  database: 'test'
 });
 
-server.listen(8080);
+db.connect((err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('연결 완료');
+});
+
+// GET 요청 처리
+app.get('/cols', (req, res) => {
+  let sql = `SELECT * FROM TEST`;
+  db.query(sql, (err, results, fields) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.json(results);
+  });
+});
+
+// POST 요청 처리
+app.post('/test', (req, res) => {
+  const data = req.body;
+  console.log('POST : ', data.name);
+  let sql = `INSERT INTO TEST(NAME) VALUE(${JSON.stringify(data.name)})`;
+    db.query(sql, (err, results, fields) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.json(results);
+  });
+  // 여기서 데이터를 사용하거나 DB에 저장할 수 있습니다.
+  
+});
+
+app.listen(port, () => {
+  console.log(`서버가 ${port} 포트에서 실행 중입니다.`);
+});
